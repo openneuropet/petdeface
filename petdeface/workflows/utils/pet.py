7742ef4fcb00f8b74041ef2c469c4738ba26bfbf
Martin Norgaard <martin.noergaard@nru.dk>
2023-05-07 22:42:37 +0200

    Update pet.py

--- petdeface/workflows/utils/pet.py
+++ petdeface/workflows/utils/pet.py
@@ -1,36 +1,46 @@
-def create_weighted_average_pet(fin, json_file, fout, frames=None):
+import json
+
+def create_weighted_average_pet(pet_file, json_file):
     
     """
     Create a time-weighted average of dynamic PET data using mid-frames
     
     Arguments
     ---------
-    fin: string
+    pet_file: string
         path to input dynamic PET volume
-    fout: string
-        path to output average volume
-    frames: list of integers
-        list of frames to be used for computing the average (indices are 0-based)
+    json_file: string
+        path to PET json file containing timing information
     """     
       
-    if not isfile(fout):
-        img = nib.load(fin)        
-        data = img.get_fdata()
+    img = nib.load(pet_file)        
+    data = img.get_fdata()
 
-        frames_start, frames_duration = get_timing(json_file)
-        
-        # Check that the specified frame interval, if any, is valid
-        if frames is None:
-            frames = range(data.shape[-1])
-        else:
-            if frames[0] < 0:
-                raise ValueError('The indice of of first frame needs to be equal or larger than 0')
-            if frames[-1] >= data.shape[-1]:
-                raise ValueError('The indice of of last frame needs to less than %i' % data.shape[-1])
+    frames_start, frames_duration = get_timing(json_file)
 
-        mid_frames = frames_start + frames_duration/2
-        wavg = np.trapz(data[..., frames], dx=np.diff(mid_frames[frames]), axis=3)/np.sum(mid_frames)
-        print('Saving average to ' + fout)
-        nib.save(nib.Nifti1Image(wavg, img.affine), fout)
-    else:
-        print('File ' + fout + ' already exists. Skipping.')
+    mid_frames = frames_start + frames_duration/2
+    wavg = np.trapz(data[..., frames], dx=np.diff(mid_frames[frames]), axis=3)/np.sum(mid_frames)
+
+    out_file = pet_file.replace('.nii.gz', '_wavg.nii.gz')
+    nib.save(nib.Nifti1Image(wavg, img.affine), out_file)
+
+    return out_file
+
+def get_timing(json_file):
+    
+    """
+    Get timing information from PET json file
+    
+    Arguments
+    ---------
+    json_file: string
+        path to PET json file containing timing information
+    """     
+    
+    with open(json_file, 'r') as f:
+        json_data = json.load(f)
+    
+    frames_start = np.array(json_data['FrameTimesStart'])
+    frames_duration = np.array(json_data['FrameDuration'])
+    
+    return frames_start, frames_duration
