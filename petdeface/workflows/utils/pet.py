FIX: update time weighted average function

petdeface/workflows/utils/pet.py
@@ -1,6 +1,9 @@
 import json
+from niworkflows.interfaces.bids import ReadSidecarJSON
+import nibabel as nib
+import numpy as np
 
-def create_weighted_average_pet(pet_file, json_file):
+def create_weighted_average_pet(pet_file, bids_dir):
     
     """
     Create a time-weighted average of dynamic PET data using mid-frames
@@ -16,31 +19,19 @@ def create_weighted_average_pet(pet_file, json_file):
     img = nib.load(pet_file)        
     data = img.get_fdata()
 
-    frames_start, frames_duration = get_timing(json_file)
+    meta = ReadSidecarJSON(in_file = pet_file, 
+                           bids_dir = bids_dir, 
+                           bids_validate = False).run()
+
+    frames_start = np.array(meta.outputs.out_dict['FrameTimesStart'])
+    frames_duration = np.array(meta.outputs.out_dict['FrameDuration'])
+
+    frames = range(data.shape[-1])
 
     mid_frames = frames_start + frames_duration/2
     wavg = np.trapz(data[..., frames], dx=np.diff(mid_frames[frames]), axis=3)/np.sum(mid_frames)
 
-    out_file = pet_file.replace('.nii.gz', '_wavg.nii.gz')
+    out_file = pet_file.replace('_pet.', '_desc-wavg_pet.')
     nib.save(nib.Nifti1Image(wavg, img.affine), out_file)
 
     return out_file
-
-def get_timing(json_file):
-    
-    """
-    Get timing information from PET json file
-    
-    Arguments
-    ---------
-    json_file: string
-        path to PET json file containing timing information
-    """     
-    
-    with open(json_file, 'r') as f:
-        json_data = json.load(f)
-    
-    frames_start = np.array(json_data['FrameTimesStart'])
-    frames_duration = np.array(json_data['FrameDuration'])
-    
-    return frames_start, frames_duration