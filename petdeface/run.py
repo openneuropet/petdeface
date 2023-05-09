19384cb3f2287b993e4e7a000593e8c4a2d1958c
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2023-05-09 17:27:10 -0400

    it runs, next is testing on other datasets and in docker

--- petdeface/run.py
+++ petdeface/run.py
@@ -8,12 +8,20 @@ import subprocess
 import pathlib
 import pprint
 import nipype.interfaces.freesurfer as fs
-import nipype.interfaces.fsl as fsl
+from typing import Union
+from nipype import Node, Function
+from nipype.interfaces.io import SelectFiles
+from petdeface.workflows.mideface import Mideface
+from petdeface.workflows.utils.pet import create_weighted_average_pet
+from nipype.interfaces.freesurfer import MRICoreg
 from nipype.interfaces.utility import IdentityInterface
 from niworkflows.utils.misc import check_valid_fs_license
 from nipype.pipeline import Node, MapNode, Workflow
 from nipype.interfaces.io import DataSink
 from bids import BIDSLayout
+
+
+__version__ = pathlib.Path(__file__).parent.parent.joinpath('version').read_text().strip()
 
 
 def locate_freesurfer_license():
@@ -85,15 +93,159 @@ def check_docker_image_exists(image_name, build=False):
     return image_exists
 
 
+def deface(args: Union[dict, argparse.Namespace]):
+    """Main function for the PET Deface workflow."""
+
+    if type(args) is dict:
+        args = argparse.Namespace(**args)
+    else:
+        args = args
+
+    if os.path.exists(args.bids_dir):
+        if not args.skip_bids_validator:
+            layout = BIDSLayout(args.bids_dir, validate=True)
+        else:
+            layout = BIDSLayout(args.bids_dir, validate=False)
+    else:
+        raise Exception('BIDS directory does not exist')
+
+    if check_valid_fs_license() is not True:
+        raise Exception('You need a valid FreeSurfer license to proceed!')
+
+    # Get all PET files
+    if args.participant_label is None:
+        args.participant_label = layout.get(suffix='pet', target='subject', return_type='id')
+
+    # create output derivatives directory
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    # create output directory for this pipeline
+    pipeline_dir = os.path.join(args.output_dir, 'petdeface')
+    if not os.path.exists(pipeline_dir):
+        os.makedirs(pipeline_dir)
+
+    infosource = Node(IdentityInterface(
+        fields = ['subject_id','session_id']),
+        name = "infosource")
+
+    sessions = layout.get_sessions()
+    if sessions:
+        infosource.iterables = [('subject_id', args.participant_label),
+                                ('session_id', sessions)]
+    else:
+        infosource.iterables = [('subject_id', args.participant_label)]
+
+    templates = {'t1w_file': 'sub-{subject_id}/anat/*_T1w.[n]*' if not sessions else 'sub-{subject_id}/*/anat/*_T1w.[n]*',
+                 'pet_file': 'sub-{subject_id}/pet/*_pet.[n]*' if not sessions else 'sub-{subject_id}/ses-{session_id}/pet/*_pet.[n]*',
+                 'json_file': 'sub-{subject_id}/pet/*_pet.json' if not sessions else 'sub-{subject_id}/ses-{session_id}/pet/*_pet.json'}
+
+    selectfiles = Node(SelectFiles(templates,
+                                   base_directory = args.bids_dir),
+                       name = "select_files")
+
+    substitutions = [('_subject_id', 'sub'), ('_session_id_', 'ses')]
+    subjFolders = [('sub-%s' % (sub), 'sub-%s' % (sub))
+                   for sub in layout.get_subjects()] if not sessions else [('sub-%s_ses-%s' % (sub, ses), 'sub-%s/ses-%s' % (sub, ses))
+                                                                           for ses in layout.get_sessions()
+                                                                           for sub in layout.get_subjects()]
+
+    substitutions.extend(subjFolders)
+
+    # clean up and create derivatives directories
+    if args.output_dir is None:
+        output_dir = os.path.join(args.bids_dir,'derivatives','petdeface')
+    else:
+        output_dir = args.output_dir
+
+    # Define nodes for hmc workflow
+
+    deface_t1w = Node(Mideface(out_file = 't1w_defaced.nii.gz',
+                               out_facemask = 'face.mask.mgz',
+                               odir = '.'),
+                      name = 'deface_t1w')
+
+    coreg_pet_to_t1w = Node(MRICoreg(),
+                            name = 'coreg_pet_to_t1w')
+
+    create_time_weighted_average = Node(Function(input_names = ['pet_file', 'bids_dir'],
+                                                 output_names = ['out_file'],
+                                                 function = create_weighted_average_pet),
+                                        name = 'create_weighted_average_pet')
+
+    create_time_weighted_average.inputs.bids_dir = args.bids_dir
+
+    deface_pet = Node(Mideface(out_file = 'pet_defaced.nii.gz',
+                               out_facemask = 'face.mask.mgz',
+                               odir = '.'),
+                      name = 'deface_pet')
+
+    create_apply_str_node = Node(Function(input_names=['t1w_defaced','facemask', 'lta_file', 'pet_file', 'bids_dir'],
+                                          output_names=['apply_str'],
+                                          function=create_apply_str),
+                                 name='create_apply_str')
+    create_apply_str_node.inputs.bids_dir = args.bids_dir
+
+    workflow = Workflow(name='deface_pet_workflow', base_dir=args.bids_dir)
+    workflow.config['execution']['remove_unnecessary_outputs'] = 'false'
+    workflow.connect([(infosource, selectfiles, [('subject_id', 'subject_id'),('session_id', 'session_id')]),
+                      (selectfiles, deface_t1w, [('t1w_file', 'in_file')]),
+                      (selectfiles, create_time_weighted_average, [('pet_file', 'pet_file')]),
+                      (selectfiles, coreg_pet_to_t1w, [('t1w_file', 'reference_file')]),
+                      (create_time_weighted_average, coreg_pet_to_t1w, [('out_file', 'source_file')]),
+                      (deface_t1w, create_apply_str_node, [('out_facemask', 'facemask')]),
+                      (coreg_pet_to_t1w, create_apply_str_node, [('out_lta_file', 'lta_file')]),
+                      (selectfiles, create_apply_str_node, [('pet_file', 'pet_file')]),
+                      (deface_t1w, create_apply_str_node, [('out_file', 't1w_defaced')]),
+                      (create_apply_str_node, deface_pet, [('apply_str', 'apply')])
+                      ])
+
+    wf = workflow.run(plugin='MultiProc', plugin_args={'n_procs' : int(args.n_procs)})
+
+    # remove temp outputs
+    shutil.rmtree(os.path.join(args.bids_dir, 'deface_pet_workflow'))
+
+
+def create_apply_str(t1w_defaced, pet_file, facemask, lta_file, bids_dir):
+    """Create string to be used for the --apply flag for defacing PET using mideface."""
+    layout = BIDSLayout(bids_dir)
+    entities = layout.parse_file_entities(pet_file)
+
+    subject = entities['subject']
+    session = entities['session']
+    out_file = f"{bids_dir}/derivatives/petdeface/sub-{subject}/ses-{session}/pet/sub-{subject}_ses-{session}_desc-defaced_pet.nii.gz"
+
+    out_lta_file = f"{bids_dir}/derivatives/petdeface/sub-{subject}/ses-{session}/pet/sub-{subject}_ses-{session}_desc-pet2anat_pet.lta"
+    out_mask_file = f"{bids_dir}/derivatives/petdeface/sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_desc-defaced_mask.mgz"
+    out_t1w_defaced = f"{bids_dir}/derivatives/petdeface/sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_desc-defaced_T1w.nii.gz"
+
+    pathlib.Path(out_file).parent.mkdir(parents=True, exist_ok=True)
+    pathlib.Path(out_lta_file).parent.mkdir(parents=True, exist_ok=True)
+    pathlib.Path(out_mask_file).parent.mkdir(parents=True, exist_ok=True)
+    pathlib.Path(out_t1w_defaced).parent.mkdir(parents=True, exist_ok=True)
+
+    shutil.copyfile(t1w_defaced, out_t1w_defaced)
+    shutil.copyfile(lta_file, out_lta_file)
+    shutil.copyfile(facemask, out_mask_file)
+
+    apply_str = f"{pet_file} {facemask} {lta_file} {out_file}"
+
+    return apply_str
+
+
 class PetDeface:
-    def __init__(self, bids_dir, output_dir=None, anat_only=False, subject="", session=""):
+    def __init__(self, bids_dir, output_dir=None, anat_only=False, subject="", session="", n_procs=2,
+                 skip_bids_validator=True):
         self.bids_dir = bids_dir
         if not output_dir:
             self.output_dir = self.bids_dir
         else:
             self.output_dir = output_dir
+        self.anat_only = anat_only
         self.subject = subject
         self.session = session
+        self.n_procs = n_procs
+        self.skip_bids_validator = skip_bids_validator
 
         # check if freesurfer license is valid
         self.fs_license = check_valid_fs_license()
@@ -109,14 +261,8 @@ class PetDeface:
         self.collect_anat()
         self.collect_pet()
 
-        # print subjects
-        print("Subjects in BIDS directory: \n{}".format(pprint.pformat(self.subjects)))
+        # run pipeline
 
-        # create nipype inputs
-        self.inputs = Node(IdentityInterface(fields=['anat', 'pet']), name="inputs")
-
-        self.datasink= Node(DataSink(base_directory=str(self.output_dir / pathlib.Path("petdeface")),
-                                     container="petdeface"), name="datasink")
 
     def collect_anat(self):
         layout = BIDSLayout(self.bids_dir)
@@ -139,27 +285,16 @@ class PetDeface:
         subjects = layout.get_subjects()
         return subjects
 
-    @staticmethod
-    def convert_to_mgz(self):
-        """
-        Converts nii.gz images to mgz format
-        :return:
-        :rtype:
-        """
-        convert = Node(fs.MRIConvert(out_type="mgz"), name="convert_to_mgz")
-        return convert
-
-    @staticmethod
-    def register_anat_to_pet(self):
-        """
-        Performs image registration of anatomical image to pet image using freesurfer's mri_robust_register
-        :return:
-        :rtype:
-        """
-
-        fs.init() # initialize freesurfer
-        fs.mri_robust_register()
-        pass
+    def run(self):
+        deface({"bids_dir": self.bids_dir,
+                "output_dir": self.output_dir,
+                "anat_only": self.anat_only,
+                "subject": self.subject,
+                "session": self.session,
+                "n_procs": self.n_procs,
+                "skip_bids_validator": self.skip_bids_validator,
+                "participant_label": None,  # this should be updated to subject?
+               })
 
 
 def cli():
@@ -175,7 +310,9 @@ def cli():
     parser.add_argument("--session", "-ses", help="The label of the session to be processed.", type=str, required=False,
                         default="")
     parser.add_argument("--docker", "-d", action="store_true", default=False, help="Run in docker container")
-    parser.add_argument("--version", "-v", action="version", version="PetDeface 0.0.1")
+    parser.add_argument('--n_procs', help='Number of processors to use when running the workflow', default=2)
+    parser.add_argument("--skip_bids_validator", action="store_true", default=False)
+    parser.add_argument("--version", "-v", action="version", version="PetDeface version {}".format(__version__))
 
     arguments = parser.parse_args()
 
@@ -258,3 +395,4 @@ if __name__ == "__main__":
 
     else:
         petdeface = PetDeface(args.input_dir, args.output_dir, args.anat_only, args.subject, args.session)
+        petdeface.run()
